774ceb6b00d504ef0e1ac1bb43cd522e71d44019
Your Name <you@example.com>
2022-05-26 16:19:32 +0900

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Weclome to AWS code pipeline!'));
+app.get('/', (req, res) => res.send('Changes after making pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
